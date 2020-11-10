b8e07a83d1ae479406b34f071745e38626629c8d
Kenneth Ofosu <kofosu89@gmail.com>
2020-11-10 17:43:31 -0500

    Create GET /hello/:name endpoint

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.get("/", (request, response) => {
   response.send( "Node.js, Express, and Postgres API" );
 });
 
+app.get("/hello/:name", (request, response) => {
+  response.send( `Hello ${request.params.name}!` );
+});
+
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);
 });
