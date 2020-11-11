bad9c97651a260131469278a919510c026d6c6b7
Kenneth Ofosu <kofosu89@gmail.com>
2020-11-11 02:02:45 -0500

    Undefined routes requested return an HTTP 404 not found status

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const morgan = require("morgan");
 const os = require("os");
 const db = require("./queries");
+
+
 const app = express();
+app.use(morgan('dev'))
 const port = 3000;
 
 app.use(bodyParser.json());
@@ -23,13 +27,18 @@ app.get("/health", (request, response) => {
     os_type: os.type(),
     os_uptime: os.uptime() + "s",
     total_memory: os.totalmem() + " bytes",
-    free_memory: os.freemem() + " bytes"
+    free_memory: os.freemem() + " bytes",
   });
 });
 
 app.get("/counts", db.countsRoute);
 
 app.delete("/counts", db.deleteRoute);
+
+app.get("*", (request, response) => {
+  const { method, url } = request;
+  response.status(404).send(`Endpoint ${method} ${url} Not Found`);
+});
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);
