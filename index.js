4c181c33f266db6554d4506531234b17794c15ac
Kenneth Ofosu <kofosu89@gmail.com>
2020-11-10 23:43:24 -0500

    Update middleware for GET /hello/:name endpoint in queries.js to use url slug to trigger database action

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const db = require("./queries");
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true,
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 app.get("/", (request, response) => {
-  response.send( "Node.js, Express, and Postgres API" );
+  response.send("Node.js, Express, and Postgres API");
 });
 
-app.get("/hello/:name", (request, response) => {
-  response.send( `Hello ${request.params.name}!` );
-});
+app.get("/hello/:name", db.helloRoute);
 
 app.get("/health", (request, response) => {
-  response.send( "Placeholder for system metrics" );
+  response.send("Placeholder for system metrics");
 });
 
 app.listen(port, () => {
--- queries.js
+++ queries.js
@@ -8,3 +8,25 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   port: process.env.PORT,
 });
+
+const helloRoute = (request, response) => {
+  const { name } = request.params;
+  const count = 1;
+
+  pool
+    .query(
+      "INSERT INTO names (name, count) VALUES ($1, $2) ON CONFLICT (name) DO UPDATE SET count= names.count + 1",
+      [name, count]
+    )
+    .then((res) => {
+      response.status(200).send(`Hello ${name}`);
+      
+    })
+    .catch((error) => {
+      throw error;
+    });
+};
+
+module.exports = {
+  helloRoute,
+};
